c0768c892af5ea52635bc9c39c4dbafb7ec018f4
gelopez21 <gelopez1984@gmail.com>
2020-11-30 23:15:16 -0600

    Updated working

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,23 +6,23 @@ import scrape_mars
 app = Flask(__name__)
 
 client = pymongo.MongoClient()
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_collection")
-db = client.mars_db
-collection = db.mars_collection
-
-
-@app.route('/scrape')
-def scrape():
-    mars = scrape_mars.scrape()
-    print("\n\n\n")
-    db.mars_collection.insert_one(mars)
-    return "scrapped data"
+mongo = PyMongo(app, uri="mongodb://localhost:27017/marsData_db")
+# db = marsData_db
+# collection = db.mars_collection
 
 @app.route("/")
 def home():
-    mars = list(db.mars_collection.find())
-    print(mars)
-    return render_template("index.html", mars = mars)
+    mars_collection = mongo.db.mars_collection.find_one()
+    return render_template("index.html", mars_collection=mars_collection)
+
+@app.route('/scrape')
+def scrape():
+    mars_results = scrape_mars.scrape()
+    mars_collection = mongo.db.mars_collection
+    
+    mars_collection.update({},mars_results, upsert=True)
+    return redirect("/", code=302)
+
 
 
 if __name__ == "__main__":
